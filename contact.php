ce2a8f3ed4709846d5d354b7d68e7aa61bf7bd22
Ryan Bridglal <social@it.ryanjb.com>
2015-04-17 19:29:04 -0400

    Update contact.php

--- contact.php
+++ contact.php
@@ -3,8 +3,8 @@ $field_name = $_POST['cf_name'];
 $field_email = $_POST['cf_email'];
 $field_message = $_POST['cf_message'];
 
-$mail_to = 'helpme@ryanjb.com';
-$subject = 'I.T. Consultants contact message '.$field_name;
+$mail_to = 'helpme@itdevz.com';
+$subject = 'I.T. Devz contact message '.$field_name;
 
 $body_message = 'From: '.$field_name."\n";
 $body_message .= 'E-mail: '.$field_email."\n";
@@ -17,14 +17,14 @@ $mail_status = mail($mail_to, $subject, $body_message, $headers);
 
 if ($mail_status) { ?>
 	<script language="javascript" type="text/javascript">
-		alert('Thank you for the message. We will contact you shortly. Dont forget, you can reach us on Facebook/Twitter!');
-		window.location = 'http://it.ryanjb.com';
+		alert('Thank you for the message. We will contact you shortly. Don't forget, you can reach us on Facebook/Twitter!');
+		window.location = 'http://www.itdevz.com';
 	</script>
 <?php
 }
 else { ?>
 	<script language="javascript" type="text/javascript">
-		alert('Sorry, your message failed. Please, send an email to helpme@ryanjb.com, Dont forget, you can reach us on Facebook/Twitter!');
+		alert('Sorry, your message failed. Please, send an email to helpme@itdevz.com, Don't forget, you can reach us on Facebook/Twitter!');
 		window.location = '/contact.htm';
 	</script>
 <?php
